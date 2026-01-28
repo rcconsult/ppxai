5f28cc1db5c52397462c67083b62326f73c4fd7b
Radovan Chytracek <radovan.chytracek@tradition.ch>
2026-01-28 11:40:36 +0100

    chore: update event_handler.py version to v1.15.0

--- ppxai/rich/event_handler.py
+++ ppxai/rich/event_handler.py
@@ -9,7 +9,7 @@ Architecture:
 - Callbacks are provided by the client for rendering
 - Business logic is centralized, UI is delegated
 
-Version: v1.14.2
+Version: v1.15.0
 """
 
 from datetime import datetime
